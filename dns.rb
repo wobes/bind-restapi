2a3a43fbd00cf30c19d85f38e4af7837a35b99b1
root <root@gregsandbox01.ncw.webapps.rr.com>
2015-05-12 22:38:54 +0000

    added abaility to add and delete CNAMES

--- dns.rb
+++ dns.rb
@@ -7,6 +7,7 @@ require 'ipaddr'
 
 # curl -X POST -H 'Content-Type: application/json' -H 'X-Api-Key: secret' -d '{ "hostname": "host12.apple.com", "ip": "1.1.1.12" }' http://localhost:4567/dns
 # curl -X DELETE -H 'Content-Type: application/json' -H 'X-Api-Key: secret' -d '{ "hostname": "host12.apple.com", "ip": "1.1.1.12" }' http://localhost:4567/dns
+# curl -X POST -H 'Content-Type: application/json' -H 'X-Api-Key: secret' -d '{ "hostname": "host12.apple.com", "alias": "alias.apple.com" }' http://localhost:4567/cname
 
 dns_params = {
   :server => '127.0.0.1',
@@ -27,6 +28,7 @@ before do
   error 401 unless env['HTTP_X_API_KEY'] =~ /secret/
 end
 
+# Manage A Records
 post '/dns' do
   request_params = JSON.parse(request.body.read)
   reverse_zone = reverse_ip(request_params["ip"])
@@ -58,6 +60,39 @@ delete '/dns' do
       update delete #{request_params["hostname"]} A
       send
       update delete #{reverse_zone} PTR
+      send
+    EOF
+    f.close_write
+  end
+  error 500 unless $? == 0
+end
+
+# Manage Cnames
+post '/cname' do
+  request_params = JSON.parse(request.body.read)
+  ttl = if request_params["ttl"].nil? then dns_params[:ttl] else request_params["ttl"] end
+
+  # Add CNAME to zones, via TCP
+  IO.popen("nsupdate -y #{dns_params[:rndc_key]}:#{dns_params[:rndc_secret]} -v", 'r+') do |f|
+    f << <<-EOF
+      server #{dns_params[:server]}
+      update add #{request_params["alias"]}. #{ttl} cname #{request_params["hostname"]}
+      send
+    EOF
+    f.close_write
+  end
+  error 500 unless $? == 0
+  status 201
+end
+
+delete '/cname' do
+  request_params = JSON.parse(request.body.read)
+
+  # Remove CNAME record zone, via TCP
+  IO.popen("nsupdate -y #{dns_params[:rndc_key]}:#{dns_params[:rndc_secret]} -v", 'r+') do |f|
+    f << <<-EOF
+      server #{dns_params[:server]}
+      update delete #{request_params["alias"]} cname
       send
     EOF
     f.close_write
